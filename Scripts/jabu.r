fbe44cf3af131ccc4b69cc91e08821bb7e22faf6
Jabulani Ncube <jabulani.ncube@mafisa.org>
2025-10-23 17:57:01 +0200

    jn

--- Scripts/jabu.r
+++ Scripts/jabu.r
@@ -2,4 +2,4 @@ library(tidyverse)
 library(readxl)
 
 
-data1 <- read_xlsx("Mafisa-R-Scripts/Data/COHORT 2 waterpoint with coordinates.xlsx")
+# data1 <- read_xlsx("Mafisa-R-Scripts/Data/COHORT 2 waterpoint with coordinates.xlsx")
